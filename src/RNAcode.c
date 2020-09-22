83dd0b3f43354b5c59d7d595cbfa68031e704ee9
Christian <cfortmann@techfak.uni-bielefeld.de>
2020-09-22 13:37:00 +0200

    removed signal debugger

--- src/RNAcode.c
+++ src/RNAcode.c
@@ -36,20 +36,6 @@
 #include "misc.h"
 #include "utils.h"
 
-void handler(int sig) {
-  void *array[10];
-  size_t size;
-
-  // get void*'s for all entries on the stack
-  size = backtrace(array, 10);
-
-  // print out all the frames to stderr
-  fprintf(stderr, "Error: signal %d:\n", sig);
-  backtrace_symbols_fd(array, size, STDERR_FILENO);
-  exit(1);
-}
-
-
 /* Global variables shared troughout the application */
 
 parameters pars; /* user options */
